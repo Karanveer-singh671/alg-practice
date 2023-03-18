a1aabbc6d3571de065a3869a55a70b5cec8226e4
Karanveer Singh <singhkaranveer671@gmail.com>
2023-03-18 15:59:50 -0700

    two pointer typed out string

--- string/TypedOutString.js
+++ string/TypedOutString.js
@@ -81,4 +81,47 @@ const compareBrute = (s, t) => {
 	return true;
 };
 
+/*
+since # will delete backwords we want to start are two pointer from the end of the strings
+not the beginning. if we deleted forwards we would initialize the pointers at the beginning
+of both strings
+*/
+const twoPointerTypedString = (s, t) => {
+	let p1 = s.length - 1;
+	let p2 = t.length - 1;
 
+	while (p1 >= 0 || p2 >= 0) {
+		if (s[p1] === '#' || t[p1] === '#') {
+			if (s[p1] === '#') {
+				let backCount = 2;
+				while (backCount > 0) {
+					p1--;
+					backCount--;
+					// we need to check if there are two or more # side by side
+					if (s[p1] === '#') {
+						backCount += 2;
+					}
+				}
+			}
+			if (t[p2] === '#') {
+				let backCount = 2;
+				while (backCount > 0) {
+					p2--;
+					backCount--;
+					// we need to check if there are two or more # side by side
+					if (t[p2] === '#') {
+						backCount += 2;
+					}
+				}
+			}
+		} else {
+			if (s[p1] !== t[p2]) {
+				return false;
+			} else {
+				p1--;
+				p2--;
+			}
+		}
+	}
+  return true
+};
