17147b1ff1b97bf8b2dfd0d37e7b22bbc9a84fe3
Karanveer Singh <singhkaranveer671@gmail.com>
2023-03-21 15:02:46 -0700

    reverse linkedList

---
+++ LinkedList/reverseLinkedList.js
@@ -0,0 +1,37 @@
+/*
+reverse a linkedList
+
+what do we return if we get null or a single node -> return null and node back respectively
+*/
+
+const reverseLinkedListSkeleton = (head) => {
+	// initialize currentNode and while loop to go thru LinkedList
+	let currentNode = head;
+	while (currentNode) {
+		// perform operation
+
+		// increment to next node
+		currentNode = currentNode.next;
+	}
+};
+
+const reverseLinkedList = (head) => {
+	// initialize currentNode and while loop to go thru LinkedList
+	let current = head;
+	let prev = null;
+	while (current) {
+		// keep track of the next node we are going to work on
+		let next = current.next;
+		// next of the current (pointer) should point to the previous value
+		current.next = prev;
+
+		// we want to now set previous (accumulated list answer) to be the current
+		prev = current;
+
+		// now we want to update our current to be the next value for the next iteration of the loop
+		current = next;
+	}
+
+	// previous will hold the linkedList we created thus far
+	return prev;
+};
