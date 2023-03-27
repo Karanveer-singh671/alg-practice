83b11e259635e484edac8abeb13ec85fce053f7b
Karanveer Singh <singhkaranveer671@gmail.com>
2023-03-27 09:39:35 -0700

    find cycle linkedList space optimization

--- LinkedList/cycledetection.js
+++ LinkedList/cycledetection.js
@@ -50,3 +50,34 @@ const findCycle = (head) => {
 	}
 	return currentNode;
 };
+
+const findCycleHareTortoise = (head) => {
+	let hare = head;
+	let tortoise = head;
+	// hare checks for tail since it moves faster
+	// keep looping until find either of two cases and break out
+	while (true) {
+		hare = hare.next;
+		tortoise = tortoise.next;
+		// has a tail, no cycle
+		if (hare === null || hare.next === null) {
+			return false;
+		} else {
+			// move hare another step (2 steps total)
+			hare = hare.next;
+		}
+		if (tortoise === hare) {
+			// cycle found
+			break;
+		}
+	}
+	let p1 = head;
+	let p2 = hare;
+	// advance pointers until they meet (start node and meeting point)
+	while (p1 !== p2) {
+		p1 = p1.next;
+		p2 = p2.next;
+	}
+  // return either p1 or p2
+	return p1;
+};
