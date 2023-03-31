2bb997b8c523990f7f7086a9c854975e99291623
Karanveer Singh <singhkaranveer671@gmail.com>
2023-03-31 11:19:03 -0700

    sort

--- Recursion/quicksort.js
+++ Recursion/quicksort.js
@@ -1,27 +1,3 @@
-/*
-Given an integer array nums and an integer k, return the kth largest element in the array.
-
-Note that it is the kth largest element in the sorted order, not the kth distinct element.
-
-You must solve it in O(n) time complexity.
-
-
-
-Example 1:
-
-Input: nums = [3,2,1,5,6,4], k = 2
-Output: 5
-Example 2:
-
-Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
-Output: 4
-
-
-Constraints:
-
-1 <= k <= nums.length <= 105
--104 <= nums[i] <= 104
-*/
 const quickSort = (array, left, right) => {
 	// if left is same as right then 1 element do not wanna perform quicksort
 	if (left < right) {
@@ -47,10 +23,4 @@ const paritition = (array, left, right) => {
 
 const swap = (array, i, j) => {
 	return ([array[i], array[j]] = [array[j], array[i]]);
-};
-// O(nlogn)
-const getKthLargest = (array, k) => {
-	const indexToFind = array.length - k;
-	quickSort(array, 0, array.length - 1);
-	return array[indexToFind];
 };
